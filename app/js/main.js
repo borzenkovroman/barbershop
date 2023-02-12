e1c817ada939e0dc8a61404b7128d7f8be123c19
borzenkovroman <borzoman99@gmail.com>
2023-02-12 22:09:01 +0200

    ready made project with adaptive for all devices, and optimized for better page speed downloading

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,4 @@
 $(function () {
-
-  $('.testimonials__item-link').on('click', function () {
-    $('.testimonials__item-text--hidden').toggleClass('active');
-    $('.testimonials__item-link').toggleClass('active');
-  });
-  
-  $('.portfolio__btn-main').on('click', function () {
-    $('.portfolio__bottom-list--hidden').toggleClass('active');
-    $('.portfolio__btn-main').toggleClass('active');
-  });
   
   $('.testimonials__list').slick({
     dots: false,
@@ -18,15 +8,15 @@ $(function () {
     nextArrow: '<button type="button" class="slick-next"><svg class="slick-icon" width="10" height="18"><use xlink:href="images/icons/sprite.svg#arrow-next"></use></svg></button>',
     fade: false,
     autoplay: true,
-    autoplaySpeed: 2000,
-    slidesToScroll: 4,
-    slidesToShow: 4,
+    autoplaySpeed: 3000,
+    slidesToScroll: 1,
+    slidesToShow: 1,
     responsive: [
       {
         breakpoint: 1200,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3
+          slidesToShow: 1,
+          slidesToScroll: 1
         }
       },
       {
@@ -60,6 +50,7 @@ $(function () {
 
   $('.menu__btn').on('click', function () {
     $('.menu').toggleClass('active');
+    $('.hero__btn').toggleClass('active');
     $(this).toggleClass('active');
     $('#body').toggleClass('lock');
   });
